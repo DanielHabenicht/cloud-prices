dc87362361412c14b908b61511e0fc0d6fff4270
DanielHabenicht <daniel-habenicht@outlook.de>
2024-08-04 02:22:49 +0000

    mvp frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { createSQLiteHTTPPool } from 'sqlite-wasm-http';
+// https://fuzzy-pancake-97q4x4pxgv6hxp59-5173.app.github.dev/node_modules/sqlite-wasm-http/dist/sqlite-worker.js?worker_file&type=classic
+
+console.log("start")
+setTimeout(async () => {
+  try {
+    const remoteURL = 
+      '/test.sqlite';
+  
+    const pool = await createSQLiteHTTPPool({ workers: 8 });
+    await pool.open(remoteURL);
+    // This will automatically use a free thread from the pool
+    const queryResult = await pool.exec('SELECT serviceName FROM prices_azure');
+    console.log(queryResult[0].row);
+    // This shutdowns the pool
+    await pool.close();
+  } catch(e){
+    console.log(e)
+  }
+}, 0);
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>
+      Hi!
+    </h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import prebundleWorkers from "vite-plugin-prebundle-workers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    // prebundleWorkers({
+    //     include: "node_modules/sqlite-wasm-http/dist/sqlite-worker.js"
+    // })
+  ],
+  // build: {
+  //   lib: {
+  //     entry: path.resolve(__dirname, "src/main.js"),
+  //     name: "ViteUmdBug"
+  //   },
+  //   rollupOptions: {
+  //     external: ["vue"],
+  //     output: {
+  //       inlineDynamicImports: true, // <== here the entry
+  //       globals: {
+  //         vue: "Vue"
+  //       }
+  //     },
+
+  //   }
+  // },
+  worker: {
+    format: 'es'
+  }
+  // worker: {
+  //   format: "iife"
+  // },
+  // optimizeDeps: {
+  //   exclude: [
+  //     "sqlite-wasm-http"
+  //   ]
+  // }
 })
