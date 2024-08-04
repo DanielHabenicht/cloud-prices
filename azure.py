f5913a6c9cdbfa3ff0c101905551c72eb9988f51
DanielHabenicht <daniel-habenicht@outlook.de>
2024-08-04 11:50:05 +0000

    update scripts

--- azure.py
+++ azure.py
@@ -1,17 +1,28 @@
 from datetime import datetime
-import logging 
+import json
+import logging
+from typing import Dict
+import uuid 
 import requests
 from requests.adapters import HTTPAdapter, Retry
-import pandas as pd
+import polars as pl
 import os.path
 import sqlite3
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+
+from database import Base, Location, Meter, PricePoint, Product, Region, Service, Sku, Type
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 logging.info("Starting")
 
 cache_file = "cache.parquet"
-use_cache = False
+use_cache = True
 export_data = True
 
 if (not use_cache) or not os.path.isfile(cache_file) :
@@ -24,51 +35,163 @@ if (not use_cache) or not os.path.isfile(cache_file) :
     s.mount('https://', HTTPAdapter(max_retries=retries))
 
     items = []
-    json = {
+    response = {
         "NextPageLink": "https://prices.azure.com/api/retail/prices?api-version=2023-01-01-preview&meterRegion='primary'"
     }
     while True:
-        if "NextPageLink" in json and json["NextPageLink"] is not None:
-            page = json["NextPageLink"]
+        if "NextPageLink" in response and response["NextPageLink"] is not None:
+            page = response["NextPageLink"]
         else:
             break
 
         logging.info(f"load page {page}")
         data = s.get(page)
 
-        json = data.json()
-        if "Items" in json:
-            items.extend(json["Items"])
+        response = data.json()
+        if "Items" in response:
+            items.extend(response["Items"])
+        # break
 
-        break
 
+    for item in items:
+        key = "savingsPlan"
+        if key in response:
+            item[key] = json.dumps(item[key])
+        else:
+            item[key] = ""            
     # Currently does not work: https://github.com/pola-rs/polars/issues/17745
     # df = pl.json_normalize(items)
-    df = pd.json_normalize(items)
+    df = pl.DataFrame(items)
+    df.write_parquet(cache_file)
 
 else:
-    df = pd.read_parquet(cache_file)
+    df = pl.read_parquet(cache_file)
 
-df = df.drop(["savingsPlan"], axis=1)
-print(df)
+# df = df.drop(["savingsPlan"])
 
-
-df.to_parquet(cache_file)
-df["import_date"] = datetime.now()
+df = df.with_columns(import_date = datetime.now())
+df = df.with_columns(
+    pl.when(pl.col(pl.String).str.len_chars() == 0)
+    .then(None)
+    .otherwise(pl.col(pl.String))
+    .name.keep()
+)
+# TODO: reduce size by transforming data into multiple tables.
 
 if export_data:
+    database_path = "frontend/public/test_new.sqlite"
+    logging.info(f"Export to {database_path}")
 
-    database_path = "frontend/public/test.sqlite"
-    # connection=sqlite3.connect(database_path)
+    engine = create_engine(f"sqlite:///{database_path}")
+    Base.metadata.create_all(engine)
 
-    # c=connection.cursor()
-    # # https://github.com/mmomtchev/sqlite-wasm-http?tab=readme-ov-file#page-size
-    # c.execute("PRAGMA JOURNAL_MODE = DELETE;")
-    # c.execute("PRAGMA page_size = 1024;")
-    # #-- Do it for every FTS table you have
-    # #-- (geospatial datasets do not use full text search)
-    # # c.execute("INSERT INTO prices_azure(prices_azure) VALUES ('optimize');")
-    # #-- Reorganize database and apply changed page size
-    # #-- Sometimes you will be surprised by the new size of your DB
-    # c.execute("VACUUM;")
-    df.to_sql("prices_azure", f"sqlite:///{database_path}", if_exists="append")
+    
+    @event.listens_for(Engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        # https://github.com/mmomtchev/sqlite-wasm-http?tab=readme-ov-file#page-size
+        cursor.execute("PRAGMA JOURNAL_MODE = DELETE;")
+        cursor.execute("PRAGMA page_size = 1024;")
+        #-- Do it for every FTS table you have
+        #-- (geospatial datasets do not use full text search)
+        # c.execute("INSERT INTO prices_azure(prices_azure) VALUES ('optimize');")
+        #-- Reorganize database and apply changed page size
+        #-- Sometimes you will be surprised by the new size of your DB
+        cursor.execute("VACUUM;")
+        cursor.close()
+
+    regions: Dict[str, Meter] = {}
+    locations: Dict[str, Location] = {}
+    types: Dict[str, Location] = {}
+    meters: Dict[str, Location] = {}
+    products: Dict[str, Location] = {}
+    skus: Dict[str, Location] = {}
+    services: Dict[str, Location] = {}
+
+    logging.info("mapping regions")
+    for region in df["armRegionName"].unique():
+        if region is not None:
+            regions[region] = Region(
+                arm_name=region
+            )
+
+    logging.info("mapping locations")
+    for location in df["location"].unique():
+        if location is not None:
+            locations[location] = Location(
+                name=location
+            )
+
+    logging.info("mapping consumption type")
+    for type in df["type"].unique():
+        types[type] = Type(
+            name=type
+        )
+
+    logging.info("mapping products")
+    product_variations = df[["productId", "productName"]].unique(["productId", "productName"])
+    if len(product_variations) != len(df["productId"].unique()):
+        raise Exception("product ids not unique")
+    for type in product_variations.rows(named=True):
+        products[type["productId"]] = Product(
+            id=type["productId"],
+            name=type["productName"],
+        )
+
+
+    logging.info("mapping services")
+    service_variations = df[["serviceId", "serviceName", "serviceFamily"]].unique(["serviceId", "serviceName", "serviceFamily"])
+    if len(service_variations) != len(df["serviceId"].unique()):
+        raise Exception("service ids not unique")
+    for type in service_variations.rows(named=True):
+        services[type["serviceId"]] = Service(
+            id=type["serviceId"],
+            name=type["serviceName"],
+            family=type["serviceFamily"],
+        )
+
+    
+    with Session(engine) as session:
+        session.bulk_save_objects(services.values())
+        session.bulk_save_objects(products.values())
+        session.bulk_save_objects(locations.values())
+        session.bulk_save_objects(regions.values())
+        session.bulk_save_objects(types.values())
+        session.commit()
+
+        regions = {x.arm_name: x.id for x in session.scalars(select(Region)).all()}
+        locations = {x.name: x.id for x in session.scalars(select(Location)).all()}
+        types = {x.name: x.id for x in session.scalars(select(Type)).all()}
+        meters = {x.name: x.id for x in session.scalars(select(Meter)).all()}
+        skus = {x.name: x.id for x in session.scalars(select(Sku)).all()}
+        # products = {x.id: x for x in session.scalars(select(Product)).all()}
+        # services = {x.id: x for x in session.scalars(select(Service)).all()}
+
+    logging.info("converting dataframe")
+    df = (df.with_columns(
+        pl.struct(pl.col('*')).map_elements(lambda row: PricePoint(
+                    unit_price=row["unitPrice"],
+                    retail_price=row["retailPrice"],
+                    tier_minimum_units=row["tierMinimumUnits"],
+                    effective_start_date=datetime.strptime(row["effectiveStartDate"], "%Y-%m-%dT%H:%M:%S%z"),
+                    unit_of_measure=row["unitOfMeasure"],
+                    is_primary=row["isPrimaryMeterRegion"],
+                    reservations=row["reservationTerm"],
+                    import_date=row["import_date"],
+                    meter_id=uuid.UUID(row['meterId']),
+                    region_id=regions[row["armRegionName"]] if row["armRegionName"] is not None else None,
+                    location_id=locations[row["location"]] if row["location"] is not None else None,
+                    consumption_type_id=types[row["type"]] if row["type"] is not None else None,
+                    service_id=row["serviceId"],
+                    product_id=row["productId"]
+
+            ))
+            .alias('object'))
+    )
+
+    
+    with Session(engine) as session:
+        session.bulk_save_objects(df["object"])
+        session.commit()
+    
+print(df)
---
+++ database.py
@@ -0,0 +1,123 @@
+from datetime import datetime
+from typing import List
+from typing import Optional
+from uuid import UUID
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+class Base(DeclarativeBase):
+    pass
+
+class PricePoint(Base):
+    __tablename__ = "azure_price_points"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    # All prices are in USD so we do not nead to persist it
+    # curreny_code: Mapped[str] = mapped_column(String(3))
+    tier_minimum_units: Mapped[float]
+    retail_price: Mapped[float]
+    unit_price: Mapped[float]
+    effective_start_date: Mapped[datetime]
+    
+    ## TODO
+    unit_of_measure: Mapped[str]
+    is_primary: Mapped[bool]
+    reservations: Mapped[Optional[str]]
+    import_date: Mapped[datetime]
+
+    consumption_type_id: Mapped[Optional[int]] = mapped_column(ForeignKey("azure_consumption_types.id"))
+    consumption_type: Mapped["Type"] = relationship(back_populates="price_points")
+
+    meter_id: Mapped[Optional[int]] = mapped_column(ForeignKey("azure_meters.id"))
+    meter: Mapped["Meter"] = relationship(back_populates="price_points")
+
+    region_id: Mapped[Optional[int]] = mapped_column(ForeignKey("azure_regions.id"))
+    region: Mapped["Region"] = relationship(back_populates="price_points")
+
+    location_id: Mapped[Optional[int]] = mapped_column(ForeignKey("azure_locations.id"))
+    location: Mapped["Location"] = relationship(back_populates="price_points")
+
+    product_id: Mapped[Optional[str]] = mapped_column(ForeignKey("azure_products.id"))
+    product: Mapped["Product"] = relationship(back_populates="price_points")
+
+    # sku: Mapped[str] = mapped_column(ForeignKey("azure_sku.id"))
+    # sku: Mapped["Sku"] = relationship(back_populates="azure_sku")
+
+    service_id: Mapped[Optional[str]] = mapped_column(ForeignKey("azure_services.id"))
+    service: Mapped["Service"] = relationship(back_populates="price_points")
+
+
+    def __repr__(self) -> str:
+        return f"PricePoint(id={self.id!r})"
+
+
+class Type(Base):
+    __tablename__ = "azure_consumption_types"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    price_points: Mapped[List["PricePoint"]] = relationship(
+        back_populates="consumption_type", cascade="all, delete-orphan"
+    )
+
+class Meter(Base):
+    __tablename__ = "azure_meters"
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    price_points: Mapped[List["PricePoint"]] = relationship(
+        back_populates="meter", cascade="all, delete-orphan"
+    )
+
+class Region(Base):
+    __tablename__ = "azure_regions"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    arm_name: Mapped[str]
+    price_points: Mapped[List["PricePoint"]] = relationship(
+        back_populates="region", cascade="all, delete-orphan"
+    )
+
+class Location(Base):
+    __tablename__ = "azure_locations"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    price_points: Mapped[List["PricePoint"]] = relationship(
+        back_populates="location", cascade="all, delete-orphan"
+    )
+
+
+class Product(Base):
+    __tablename__ = "azure_products"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+
+    price_points: Mapped[List["PricePoint"]] = relationship(
+        back_populates="product", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self) -> str:
+        return f"Product(id={self.id!r})"
+    
+class Sku(Base):
+    __tablename__ = "azure_skus"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+
+    def __repr__(self) -> str:
+        return f"Sku(id={self.id!r})"
+    
+class Service(Base):
+    __tablename__ = "azure_services"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    # TODO:
+    family: Mapped[Optional[str]] = mapped_column(String(30))
+
+    price_points: Mapped[List["PricePoint"]] = relationship(
+        back_populates="service", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self) -> str:
+        return f"Service(id={self.id!r})"
